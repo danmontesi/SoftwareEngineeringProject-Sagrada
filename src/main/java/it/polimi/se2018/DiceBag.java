ad2f63f03188981f38d443017c2d8ce53a59208f
alessiomolinari <alessio.molinari96@gmail.com>
2018-04-24 17:10:51 +0200

    implement Die and DiceBag classes

--- src/main/java/it/polimi/se2018/DiceBag.java
+++ src/main/java/it/polimi/se2018/DiceBag.java
@@ -1,19 +1,27 @@
 package it.polimi.se2018;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DiceBag {
     private ArrayList<Die> dice;
 
+    //extract a random die from the bag
     public Die extractDie(){
-        return dice.remove(0);
+        int index = ThreadLocalRandom.current().nextInt(0,  dice.size());
+        return dice.remove(index);
     }
 
-    //insert a die in the diceBag
+    //insert a die in the diceBag (queued in last position)
     public void insertDie(Die die){
         dice.add(die);
     }
-    //DA IMPLEMENTARE
+
+    //switch a toBeSwitched die with a random die in diceBag
     public Die switchDie (Die toBeSwitched){
+        int index = ThreadLocalRandom.current().nextInt(0,  dice.size());
+        Die temp = dice.get(index);
+        dice.set(index, toBeSwitched);
+        return temp;
     }
 }
--- src/main/java/it/polimi/se2018/Die.java
+++ src/main/java/it/polimi/se2018/Die.java
@@ -1,24 +1,30 @@
 package it.polimi.se2018;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Die {
     private int id;
     private COLOR color;
     private int value;
 
     public void flip(){
-
+        this.value = 7 - this.value;
     }
 
     public void roll(){
-
+        this.value = ThreadLocalRandom.current().nextInt(1, 7);
     }
 
     public void increaseByOne(){
-
+        if (this.value < 6){
+            this.value +=1;
+        }
     }
 
     public void decreaseByOne(){
-
+        if (this.value > 1){
+            this.value -=1;
+        }
     }
 
     public void setValue(int value){
