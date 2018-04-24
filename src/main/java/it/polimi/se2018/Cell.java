6db24b82b24dabdd85c660e2332f237e47f5809e
migo3 <nives.migotto@gmail.com>
2018-04-24 16:04:50 +0200

    Add common classes

--- src/main/java/it/polimi/se2018/Cell.java
+++ src/main/java/it/polimi/se2018/Cell.java
@@ -1,4 +1,31 @@
 package it.polimi.se2018;
 
 public class Cell {
+    private COLOR colorConstraint;
+    private int numberConstraint;
+    private Die associatedDie;
+
+    public void setAssociatedDie(Die toBeSetDie){
+        this.associatedDie = toBeSetDie;
+    }
+
+    public Die removeDie(){
+
+    }
+
+    public Die swipeDie(Die toSwipeDie){
+
+    }
+
+    public Die getAssociatedDie(){
+        return associatedDie;
+    }
+
+    public COLOR getColorConstraint(){
+        return colorConstraint;
+    }
+
+    public int getNumberConstraint(){
+        return numberConstraint;
+    }
 }
---
+++ src/main/java/it/polimi/se2018/DraftPool.java
@@ -0,0 +1,27 @@
+package it.polimi.se2018;
+
+import java.util.ArrayList;
+
+public class DraftPool {
+    private ArrayList<Die> dice;
+
+    public Die takeDie(int diePosition){
+
+    }
+
+    public Die switchDie(int diePosition, Die toBeSwitched){
+
+    }
+
+    public void placeDie(Die toBePlaced){
+
+    }
+
+    public void rollDice(){
+
+    }
+
+    public Die getDie(){
+
+    }
+}
