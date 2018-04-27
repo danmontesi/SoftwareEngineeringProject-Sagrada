fa4fc064ecd6f6a0aef60a40365d993cc3d78337
migo3 <nives.migotto@gmail.com>
2018-04-27 18:59:35 +0200

    Add common classes

--- src/main/java/it/polimi/se2018/RoundTrack.java
+++ src/main/java/it/polimi/se2018/RoundTrack.java
@@ -7,19 +7,23 @@ public class RoundTrack {
     private ArrayList<Cell> roundCells;
 
     public Die removeDie(int diePosition){
-
+        return roundCells.get(diePosition).removeDie();
     }
 
     public Die switchDie(int diePosition, Die toBeSwitched){
-
+        return roundCells.get(diePosition).switchDie(toBeSwitched);
     }
 
-    public void placeDie(Die toBePut){
-
+    public void placeDie(Die toBePlaced) {
+        int i = 0;
+        while (roundCells.get(i) != null) {
+            i++;
+        }
+        roundCells.get(i).setAssociatedDie(toBePlaced);
     }
 
     public Die getDie(int cellNumber){
-
+        return roundCells.get(cellNumber).getAssociatedDie();
     }
 
 }
