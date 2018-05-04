75e50068ebfb3349f8d93cab57876fd43d217bb6
danmontesi <daniele.montesi@mail.polimi.it>
2018-05-04 23:29:01 +0200

    Added first test

---
+++ src/test/java/it/polimi/se2018/MatchTest/GameMatchTest.java
@@ -0,0 +1,50 @@
+package it.polimi.se2018.MatchTest;
+
+import it.polimi.se2018.*;
+import it.polimi.se2018.MVC.Controller;
+import it.polimi.se2018.network.ClientConnection;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class GameMatchTest {
+
+   Model model;
+
+   Player p1 = new Player("Gabriele");
+   Player p2 = new Player("Antonio");
+
+   @Before
+    public void setUp(){
+       ArrayList<Player> arrayPlayer = new ArrayList<>();
+       arrayPlayer.add(p1);
+       arrayPlayer.add(p2);
+       model = Model.getInstance(arrayPlayer);
+
+       assertEquals(2, model.getGamePlayers().size());
+       assertEquals(2, model.getConnectedPlayers().size());
+       assertEquals(3, model.getExtractedPublicObjectiveCard());
+       assertEquals(3, model.getExtractedToolCard());
+       assertEquals(10, model.getGameRounds());
+   }
+
+   @Test
+    public void newTurn(){
+        model.nextRound();
+
+        assertEquals(9, model.getGameRounds().size());
+   }
+
+   @Test
+    public void newTurn2(){
+        model.nextRound();
+
+        assertEquals(8, model.getGameRounds().size());
+   }
+
+
+}
