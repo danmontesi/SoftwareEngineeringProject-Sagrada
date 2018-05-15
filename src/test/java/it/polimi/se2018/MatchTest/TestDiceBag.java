87081da33fbabe9b52a6055878c1d04ba8e0e261
migo3 <313novantasei>
2018-05-15 18:21:16 +0200

    testing

--- src/test/java/it/polimi/se2018/MatchTest/TestDiceBag.java
+++ src/test/java/it/polimi/se2018/MatchTest/TestDiceBag.java
@@ -1,6 +1,5 @@
 package it.polimi.se2018.MatchTest;
 
-import it.polimi.se2018.COLOR;
 import it.polimi.se2018.DiceBag;
 import it.polimi.se2018.Die;
 import org.junit.Before;
@@ -18,7 +17,10 @@ public class TestDiceBag {
 
     @Test
     public void testExtractDie(){
+        int size1 = diceBag.getDice().size();
         Die temp = diceBag.extractDie();
+        int size2 = diceBag.getDice().size() + 1;
+        assertEquals(size1, size2);
         Die temp2;
         assertNotNull(temp);
         for (int i=0; i<diceBag.getDice().size(); i++){
@@ -31,12 +33,15 @@ public class TestDiceBag {
     public void testInsertDie(){
         Die die = diceBag.extractDie();
         int c=0;
+        int size1 = diceBag.getDice().size();
         for(int i=0; i<diceBag.getDice().size(); i++){
             if (diceBag.getDice().get(i).getColor().equals(die.getColor())) {
                 c++;
             }
         }
         diceBag.insertDie(die);
+        int size2 = diceBag.getDice().size() - 1;
+        assertEquals(size1, size2);
         int c2=0;
         for(int i=0; i<diceBag.getDice().size(); i++){
             if (diceBag.getDice().get(i).getColor().equals(die.getColor())) {
@@ -48,9 +53,12 @@ public class TestDiceBag {
         }
     }
 
-    /*@Test
+    @Test
     public void testSwitchDie(){
-
+        Die temp = diceBag.extractDie();
+        int size1 = diceBag.getDice().size();
+        diceBag.switchDie(temp);
+        int size2 = diceBag.getDice().size();
+        assertEquals(size1, size2);
     }
-    */
 }
