308ec6724fbaab7a6d159dc5e193ae9f5dcfd54f
danmontesi <daniele.montesi@mail.polimi.it>
2018-06-23 11:14:27 +0200

    Implement RefreshBoard for new reconnected players & disconnection notification

--- src/main/java/it/polimi/se2018/network/server/rmi/RMIVirtualClient.java
+++ src/main/java/it/polimi/se2018/network/server/rmi/RMIVirtualClient.java
@@ -20,7 +20,6 @@ public class RMIVirtualClient implements ClientConnection {
     @Override
     public void notifyClient(ServerToClientCommand command) {
 
-        //TODO: MAGARI SENZA THREAD
             new Thread() {
                 public void run() {
                     try {
@@ -32,7 +31,7 @@ public class RMIVirtualClient implements ClientConnection {
              */
                         String disconnecting;
                         for (Map.Entry<String, ClientConnection> entry : Server.getConnectedClients().entrySet()) {
-                            if (entry.getValue().equals(this)) {
+                            if (entry.getValue().equals(this)) {  //TODO: con il thread non mi prende this come connessione! COme risolvo?
                                 disconnecting = entry.getKey();
                                 Server.disconnectClient(disconnecting);
                                 System.out.println("client " + entry.getKey() + " disconnected");
