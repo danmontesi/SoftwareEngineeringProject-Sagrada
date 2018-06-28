f6b6f1bd6cf83a52f65d7fd004c622e5da4e5f3c
danmontesi <daniele.montesi@mail.polimi.it>
2018-06-28 14:56:03 +0200

    Fix little error in protection parameters

--- src/main/java/it/polimi/se2018/commands/server_to_client_command/ServerToClientCommand.java
+++ src/main/java/it/polimi/se2018/commands/server_to_client_command/ServerToClientCommand.java
@@ -8,7 +8,7 @@ public abstract class ServerToClientCommand implements Serializable {
     /**
      * This is the abstract class representing all possible command from Server to view
      */
-    private String message;
+    protected String message;
 
     /**
      * The Constant serialVersionUID.
