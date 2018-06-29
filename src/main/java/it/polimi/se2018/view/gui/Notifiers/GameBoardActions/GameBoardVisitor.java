ed2bcc76f69459a469f28d540b1cf8fef0ba7f66
migo3 <313novantasei>
2018-06-29 19:46:51 +0200

    Tool Cards added to GUI + Refactor

--- src/main/java/it/polimi/se2018/view/gui/Notifiers/GameBoardActions/GameBoardVisitor.java
+++ src/main/java/it/polimi/se2018/view/gui/Notifiers/GameBoardActions/GameBoardVisitor.java
@@ -9,7 +9,11 @@ public interface GameBoardVisitor {
     void visitGameBoardAction(WPCUpdate wpcUpdate);
     void visitGameBoardAction(TokensUpdate tokensUpdate);
     void visitGameBoardAction(DraftPoolRoundTrackUpdate draftPoolUpdate);
-    void visitGameBoardAction(ToolCardUse toolCardUse);
+    void visitGameBoardAction(AnotherAction anotherAction);
+    void visitGameBoardAction(IncreaseDecrease increaseDecrease);
+    void visitGameBoardAction(DieValue dieValue);
+    void visitGameBoardAction(PlaceDie placeDie);
+    void visitGameBoardAction(PickDie pickDie);
     void visitGameBoardAction(TimeUp timeUp);
     void visitGameBoardAction(Message message);
 }
--- src/main/java/it/polimi/se2018/view/gui/Notifiers/GameBoardActions/PlaceDie.java
+++ src/main/java/it/polimi/se2018/view/gui/Notifiers/GameBoardActions/PlaceDie.java
@@ -1,6 +1,6 @@
 package it.polimi.se2018.view.gui.Notifiers.GameBoardActions;
 
-public class TimeUp implements GameBoardAction {
+public class PlaceDie implements GameBoardAction {
     @Override
     public void acceptGameBoardVisitor(GameBoardVisitor gameBoardVisitor) {
         gameBoardVisitor.visitGameBoardAction(this);
