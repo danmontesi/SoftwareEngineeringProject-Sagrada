b0dc0526b4943ba45f2537fdd021e0f1462d977c
alessiomolinari <alessio.molinari96@gmail.com>
2018-07-01 16:28:32 +0200

    fix action

--- src/main/java/it/polimi/se2018/model/Action.java
+++ src/main/java/it/polimi/se2018/model/Action.java
@@ -5,15 +5,6 @@ public class Action {
     private String parameter;
     private String parameter2;
 
-    public Action(ACTION_TYPE type) {
-        this.type = type;
-    }
-
-    public Action(ACTION_TYPE type, String parameter) {
-        this.type = type;
-        this.parameter = parameter;
-    }
-
     public Action(ACTION_TYPE type, String parameter, String parameter2) {
         this.type = type;
         this.parameter = parameter;
@@ -24,23 +15,11 @@ public class Action {
         return type;
     }
 
-    public void setType(ACTION_TYPE type) {
-        this.type = type;
-    }
-
     public String getParameter() {
         return parameter;
     }
 
-    public void setParameter(String parameter) {
-        this.parameter = parameter;
-    }
-
     public String getParameter2() {
         return parameter2;
-    }
-
-    public void setParameter2(String parameter2) {
-        this.parameter2 = parameter2;
     }
 }
--- src/test/java/it/polimi/se2018/MatchTest/viewTest/CliTest.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.se2018.MatchTest.viewTest;
-
-public class CliTest {
-
-
-
-}
